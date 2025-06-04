1ba14347956f7e680bf911f04f234604e43dd751
qinmingyuan <mingyuan0715@foxmail.com>
2025-06-04 10:39:40 +0800

    refresh

--- app/models/one_ai/ext/reply.rb
+++ app/models/one_ai/ext/reply.rb
@@ -8,6 +8,11 @@ module OneAi
       attribute :auto_reply, :boolean, default: true
       attribute :last_replied_at, :datetime
       attribute :last_message, :string
+
+      after_save :refresh_users_box, if: -> { saved_change_to_last_replied_at? }
+    end
+
+    def refresh_users_box
     end
 
   end
